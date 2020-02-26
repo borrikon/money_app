b182bf58145616773e05ee47669f79d0289e04a6
borusjeweler <50241272+borusjeweler@users.noreply.github.com>
2020-02-26 10:44:06 +0200

    Updates

---
+++ precache-manifest.30eb7825cdbd3d43ab0835f0cb164b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad48a1c56407f7168c05fef77e807d96",
+    "url": "/borrikon/money_app/tree/master/index.html"
+  },
+  {
+    "revision": "9d5883a6d773fc041c9a",
+    "url": "/borrikon/money_app/tree/master/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8c1c720d591d78d5a4d4",
+    "url": "/borrikon/money_app/tree/master/static/css/main.03413501.chunk.css"
+  },
+  {
+    "revision": "9d5883a6d773fc041c9a",
+    "url": "/borrikon/money_app/tree/master/static/js/2.6897ac26.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/borrikon/money_app/tree/master/static/js/2.6897ac26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1c720d591d78d5a4d4",
+    "url": "/borrikon/money_app/tree/master/static/js/main.524d1961.chunk.js"
+  },
+  {
+    "revision": "948053dc85ad11688d29",
+    "url": "/borrikon/money_app/tree/master/static/js/runtime-main.e8111920.js"
+  }
+]);
